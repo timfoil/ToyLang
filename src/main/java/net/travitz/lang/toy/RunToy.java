c2985575f2fa9cecf7f02a07b31002488621a902
timfoil <tim@travitz.net>
2020-01-22 00:30:34 -0500

    Create basic lexer/parser hello world

---
+++ src/main/java/net/travitz/lang/toy/RunToy.java
@@ -0,0 +1,22 @@
+package net.travitz.lang.toy;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+import java.io.IOException;
+
+public class RunToy {
+    public static void main(String[] args) {
+        try {
+            CharStream fileStream = CharStreams.fromFileName(args[0]);
+            ToyLangLexer toyLex = new ToyLangLexer(fileStream);
+            CommonTokenStream toyTokenStream = new CommonTokenStream(toyLex);
+            ToyLangParser toyParse = new ToyLangParser(toyTokenStream);
+            System.out.println(toyParse.functions().toStringTree(toyParse));
+            System.out.println("hi");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
