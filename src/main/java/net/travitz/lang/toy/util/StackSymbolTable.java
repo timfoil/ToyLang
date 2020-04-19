1f27e0779a48e7478e37bd71de41585fb37b8858
timfoil <tim@travitz.net>
2020-04-19 19:16:31 -0400

    Fix exitScope() not actually leaving the scope

--- src/main/java/net/travitz/lang/toy/util/StackSymbolTable.java
+++ src/main/java/net/travitz/lang/toy/util/StackSymbolTable.java
@@ -63,6 +63,8 @@ public class StackSymbolTable implements SymbolTable {
         if (scopeStack.empty()) {
             throw new RuntimeException("No scopes left to exit");
         }
+        scopeLevel--;
+        scopeStack.pop();
     }
 
     @Override
