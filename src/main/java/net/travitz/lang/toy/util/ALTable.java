d530046011555a3084802ee325cdecc9ecdeb350
timfoil <tim@travitz.net>
2020-04-19 20:39:35 -0400

    Create a symbol-table where you can reference previous/enclosing scopes

---
+++ src/main/java/net/travitz/lang/toy/util/ALTable.java
@@ -0,0 +1,68 @@
+package net.travitz.lang.toy.util;
+
+import java.util.ArrayList;
+
+/**
+ * Symbol table implementation that uses an ArrayList as the backing data-structure
+ */
+public class ALTable implements SymbolTable {
+
+    /**
+     * Top level scope, scope level 0
+     */
+    Scope fileScope;
+
+    /**
+     * ArrayList containing all scopes in current context
+     */
+    ArrayList<Scope> scopeList;
+
+    /**
+     * Default constructor
+     */
+    public ALTable() {
+        fileScope = new Scope();
+        scopeList = new ArrayList<>();
+        scopeList.add(fileScope);
+    }
+
+    @Override
+    public void addToFileScope(String symbol, String type) {
+        fileScope.addToScope(symbol, type);
+    }
+
+    @Override
+    public void addToScope(String symbol, String type) {
+        fileScope.addToScope(symbol, type);
+    }
+
+    @Override
+    public void enterScope() {
+        scopeList.add(new Scope());
+    }
+
+    @Override
+    public void exitScope() {
+        // throw error if we try to exit file-level scope
+        if (scopeList.size() <= 1) {
+            throw new RuntimeException("No scopes left to exit");
+        }
+
+        scopeList.remove(scopeList.size() - 1);
+    }
+
+    @Override
+    public String lookupSymbol(String symbol) {
+        return scopeList.get(scopeList.size() - 1).lookupSymbol(symbol);
+    }
+
+    @Override
+    public boolean symbolExistsInCurrentScope(String symbol) {
+        return scopeList.get(scopeList.size() - 1).symbolExistsInScope(symbol);
+    }
+
+    @Override
+    public int scopeLevel() {
+        return scopeList.size() - 1;
+    }
+}
