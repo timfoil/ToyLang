00e7c35e47a1d8470e18647597d0bf9f673bec76
timfoil <tim@travitz.net>
2020-04-20 22:21:55 -0400

    Add getter for symbols in Scope

--- src/main/java/net/travitz/lang/toy/util/Scope.java
+++ src/main/java/net/travitz/lang/toy/util/Scope.java
@@ -1,22 +1,27 @@
 package net.travitz.lang.toy.util;
 
 import java.util.HashMap;
+import java.util.Set;
 
 /**
  * Scope implementation. Basically a wrapper around a HashMap to improve code readability for symbol tables
  */
 public class Scope {
-    HashMap<String, String> symbols = new HashMap<>();
+    HashMap<String, String> symbolTypeMap = new HashMap<>();
 
     public void addToScope(String symbol, String type) {
-        symbols.put(symbol, type);
+        symbolTypeMap.put(symbol, type);
     }
 
     public boolean symbolExistsInScope(String symbol) {
-        return symbols.containsKey(symbol);
+        return symbolTypeMap.containsKey(symbol);
     }
 
     public String lookupSymbol(String symbol) {
-        return symbols.get(symbol);
+        return symbolTypeMap.get(symbol);
+    }
+
+    public Set<String> getSymbolsMap() {
+        return symbolTypeMap.keySet();
     }
 }
