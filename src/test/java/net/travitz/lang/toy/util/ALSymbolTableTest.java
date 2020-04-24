9f8b697294c31cb564303f7d5194cd3fb0135897
timfoil <tim@travitz.net>
2020-04-23 21:09:32 -0400

    create a test for ALsymbolTable that maybe has a bit too much going on

--- src/test/java/net/travitz/lang/toy/util/ALSymbolTableTest.java
+++ src/test/java/net/travitz/lang/toy/util/ALSymbolTableTest.java
@@ -26,4 +26,61 @@ public class ALSymbolTableTest {
         ALSymbolTable cut = new ALSymbolTable();
         cut.exitScope();
     }
+
+    @Test
+    public void addToScopeIT() {
+        ALSymbolTable cut = new ALSymbolTable();
+
+        String type0 = "0";
+        String symbol = "b";
+        String symbolOther = "other";
+
+        cut.addToScope(symbol, type0);
+        cut.addToScope(symbolOther, type0);
+
+        assertEquals(cut.lookupSymbol(symbol), type0);
+        assertEquals(cut.lookupSymbol(symbolOther), type0);
+
+        String type0b = "0b";
+        cut.addToScope(symbol, type0b);
+
+        assertEquals(cut.lookupSymbol(symbol), type0b);
+
+        cut.enterScope(); // Enter tier 1
+
+        assertFalse(cut.symbolExistsInCurrentScope(symbol));
+        assertFalse(cut.symbolExistsInCurrentScope(symbolOther));
+
+        String type1 = "1";
+        cut.addToScope(symbol, type1);
+        assertEquals(cut.lookupSymbol(symbol), type1);
+
+        assertTrue(cut.symbolExistsInCurrentScope(symbol));
+        assertFalse(cut.symbolExistsInCurrentScope(symbolOther));
+
+        String type1b = "1b";
+        cut.addToScope(symbol, type1b);
+        assertEquals(cut.lookupSymbol(symbol), type1b);
+
+        cut.enterScope(); // Enter tier 2
+
+        assertFalse(cut.symbolExistsInCurrentScope(symbol));
+        assertFalse(cut.symbolExistsInCurrentScope(symbolOther));
+
+        String type2 = "2";
+        cut.addToScope(symbol, type2);
+        assertEquals(cut.lookupSymbol(symbol), type2);
+
+        assertTrue(cut.symbolExistsInCurrentScope(symbol));
+        assertFalse(cut.symbolExistsInCurrentScope(symbolOther));
+
+        String type2b = "2b";
+        cut.addToScope(symbol, type2b);
+        assertEquals(cut.lookupSymbol(symbol), type2b);
+
+        cut.exitScope();
+        assertEquals(cut.lookupSymbol(symbol), type1b);
+        cut.exitScope();
+        assertEquals(cut.lookupSymbol(symbol), type0b);
+    }
 }
