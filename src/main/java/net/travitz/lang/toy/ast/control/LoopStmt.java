99b98d469f2cb131317cb600fe6d496a543d6f40
timfoil <tim@travitz.net>
2020-04-26 20:45:49 -0400

    Create a enum to determine the type of loop being represented

--- src/main/java/net/travitz/lang/toy/ast/control/LoopStmt.java
+++ src/main/java/net/travitz/lang/toy/ast/control/LoopStmt.java
@@ -8,6 +8,10 @@ import java.util.ArrayList;
 public class LoopStmt implements Statement {
     ArrayList<Statement> loopStmts;
 
+    LoopType loopType;
+
+    private enum LoopType { FOR, WHILE, LOOP }
+
     @Override
     public String getIR() {
         return null;
