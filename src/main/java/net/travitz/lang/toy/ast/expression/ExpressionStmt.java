8893ef715f3c447c1e4e7db51968a43e15556440
timfoil <tim@travitz.net>
2020-04-27 22:47:47 -0400

    Create ExpressionStmt class

---
+++ src/main/java/net/travitz/lang/toy/ast/expression/ExpressionStmt.java
@@ -0,0 +1,16 @@
+package net.travitz.lang.toy.ast.expression;
+
+import net.travitz.lang.toy.ast.Statement;
+import net.travitz.lang.toy.ast.ToyType;
+
+public class ExpressionStmt implements Statement {
+    @Override
+    public String getIR() {
+        return null;
+    }
+
+    @Override
+    public ToyType getExpressionReturnVal() {
+        return null;
+    }
+}
