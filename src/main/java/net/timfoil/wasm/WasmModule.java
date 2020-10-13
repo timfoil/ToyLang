01c194ca727b051d6da91f258e503f86ef94e83b
timfoil <tim@travitz.net>
2020-10-12 23:11:52 -0400

    add library loading code

--- src/main/java/net/timfoil/wasm/WasmModule.java
+++ src/main/java/net/timfoil/wasm/WasmModule.java
@@ -2,6 +2,9 @@ package net.timfoil.wasm;
 
 public class WasmModule {
 
+    // load wasm_module_impl.so or wasm_module_imple.dll (unix vs windows)
+    static { System.loadLibrary("wasm_module_impl"); }
+
     private long module;
 
     native private static long getModule();
